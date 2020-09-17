c403dc7258cf6fd635d9dc13419ca236d556fc51
Jorge Mario <jmferna91@gmail.com>
2020-09-16 21:51:48 -0400

    black curtain and text fade in welcome page

--- resources/js/app.js
+++ resources/js/app.js
@@ -21,12 +21,22 @@ window.Vue = require('vue');
 
 Vue.component('example-component', require('./components/ExampleComponent.vue').default);
 
+Vue.component('welcome', require('./components/Welcome.vue').default);
+
 /**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
-const app = new Vue({
-    el: '#app',
-});
+// const app = new Vue({
+//     el: '#app',
+// });
+
+
+
+window.onload = function () {
+    const welcome = new Vue({
+        el: '#welcome',
+    });
+}
---
+++ resources/js/components/Welcome.vue
@@ -0,0 +1,41 @@
+<template>
+    <div id="curtain">
+        <div v-bind:class="[fadeIn == true ? 'makeVisible': 'makeInvisible' ]">
+
+            <div class="container" style="padding-top: 25%">
+
+                <!-- main -->
+                <span class="row">
+                    <span class="col">
+                        <h1>My Portfolio</h1>
+                        <h2>Jorge Mario Fernandez</h2>
+                    </span>
+                    <span class="col">
+                    
+                    </span>
+                </span>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Welcome Component mounted.')
+        },
+        updated(){
+            console.log("updated");
+        },
+        data: () => ({
+            fadeIn: true,
+        }),
+        methods: {
+            fade(){
+                this.fadeIn = !this.fadeIn;
+                console.log(this.fadeIn);
+            },
+            
+        }
+    }
+</script>
